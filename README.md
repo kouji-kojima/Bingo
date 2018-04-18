260d0e885da7deeb161f33379928c6d0203e7f19
kouji kojima <kouji.kojima@toshiba.co.jp>
2018-04-18 13:47:06 +0900

    修正

--- README.md
+++ README.md
@@ -4,40 +4,11 @@
 実行前準備
 ----------
 
-1.Bingo5x5Appを修正してください。
-
-```
-/** 何枚ビンゴを作成するか？ How many Bingo ? */
-private static int BINGO_NUM = 36;
-
-/** ＤＢ情報 */
-private static String DB_INFO = "jdbc:oracle:thin:@IP:PORT:ORCL"; // 今回はオラクルのサンプルです。
-private static String DB_USER = "user";
-private static String DB_PASS = "pass";
-
-/** ビンゴの中に埋める文字を格納しているテーブル。IDには1～シーケンスに登録しておく。 */
-private static String BINGO_TARGET_SQL = "SELECT id, target_name FROM BINGO_TARGET_TBL";
-
-/** ビンゴ出力先 */
-private static String BINGO_OUTPUT_PATH = "C:\\temp\\bingo.csv";
-```
-
-2.BINGO\_TARGET\_TBLテーブルを作成してください。
-
-```
-CREATE TABLE BINGO_TARGET_TBL
-(
-    ID                             NUMBER NOT NULL,
-    TARGET_NAME                    VARCHAR2(100),
-    CONSTRAINT BINGO_TARGET_PK PRIMARY KEY (ID) USING INDEX
-)
-```
-
-3.テーブルにビンゴのマスの中に表示するデータを事前に入れてください。
+bingo_target.txt を 作成し、jarと同じディレクトリにおいてください。
 
 ID には、1から順番にシーケンシャルにデータを入れてください。
 
-|ID    |TARGET_NAME |
+|      |            |
 |-----:|:----------:|
 |1     |taro        |
 |2     |jiro        |
@@ -50,5 +21,6 @@ ID には、1から順番にシーケンシャルにデータを入れてくだ�
 
 ```
 mvn package
-java -jar bingo-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+cp bingo_target.txt target/
+java -jar target/bingo-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 ```
--- src/main/java/kk/bingo/Bingo5x5App.java
+++ src/main/java/kk/bingo/Bingo5x5App.java
@@ -16,15 +16,19 @@ import java.util.Random;
 
 public class Bingo5x5App {
 
-	/** 何枚ビンゴを作成するか？ How many Bingo ? */
-	private static int BINGO_NUM = 36;
-
 	/** ビンゴ出力先 */
 	private static String BINGO_OUTPUT_PATH = "C:\\temp\\bingo.tsv";
 
 	public static void main(String[] args) throws Exception {
+
+		if (args == null || args.length == 0) {
+			System.out.println("How many BINGOs? Please enter the required number in the first argument.");
+			return;
+		}
+
 		var bingoList = new ArrayList<ArrayList<BingoTarget>>();
-		for (int i = 0; i < BINGO_NUM; i++) {
+
+		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
 			/** ランダム数（今回は真ん中だけ空欄にする為、24回ランダムでデータを取得する）*/
 			int randamNum = 24;
 			bingoList.add(pick(randamNum));
@@ -69,31 +73,6 @@ public class Bingo5x5App {
 				}
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return allBingoTargets;
-	}
-
-	/** ＤＢ情報 */
-	private static String DB_INFO = "jdbc:oracle:thin:@IP:PORT:ORCL";
-	private static String DB_USER = "user";
-	private static String DB_PASS = "pass";
-
-	/** ビンゴの中に埋める文字を格納しているテーブル。IDには1～シーケンスに登録しておく。 */
-	private static String BINGO_TARGET_SQL = "SELECT id, target_name FROM BINGO_TARGET_TBL";
-
-	private static ArrayList<BingoTarget> getAllFromDbBingoTargets() {
-		var allBingoTargets = new ArrayList<BingoTarget>();
-		try {
-			try (
-					var con = DriverManager.getConnection(DB_INFO, DB_USER, DB_PASS);
-					var ps = con.prepareStatement(BINGO_TARGET_SQL);
-					var result = ps.executeQuery();) {
-				while (result.next()) {
-					allBingoTargets.add(new BingoTarget(result.getInt("id"), result.getString("target_name")));
-				}
-			}
-		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return allBingoTargets;
@@ -163,5 +142,30 @@ public class Bingo5x5App {
 		} catch (IOException e2) {
 			e2.printStackTrace();
 		}
+	}
+
+	/** ＤＢ情報 */
+	private static String DB_INFO = "jdbc:oracle:thin:@IP:PORT:ORCL";
+	private static String DB_USER = "user";
+	private static String DB_PASS = "pass";
+
+	/** ビンゴの中に埋める文字を格納しているテーブル。IDには1～シーケンスに登録しておく。 */
+	private static String BINGO_TARGET_SQL = "SELECT id, target_name FROM BINGO_TARGET_TBL";
+
+	private static ArrayList<BingoTarget> getAllFromDbBingoTargets() {
+		var allBingoTargets = new ArrayList<BingoTarget>();
+		try {
+			try (
+					var con = DriverManager.getConnection(DB_INFO, DB_USER, DB_PASS);
+					var ps = con.prepareStatement(BINGO_TARGET_SQL);
+					var result = ps.executeQuery();) {
+				while (result.next()) {
+					allBingoTargets.add(new BingoTarget(result.getInt("id"), result.getString("target_name")));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return allBingoTargets;
 	}
 }

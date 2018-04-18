1fe123b7c88c79a526a846f3bd092743c73a3ec8
kouji kojima <kouji.kojima@toshiba.co.jp>
2018-04-18 14:10:55 +0900

    修正

--- src/main/java/kk/bingo/Bingo5x5App.java
+++ src/main/java/kk/bingo/Bingo5x5App.java
@@ -8,25 +8,33 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Bingo5x5App {
 
 	/** ビンゴ出力先 */
-	private static String BINGO_OUTPUT_PATH = "C:\\temp\\bingo.tsv";
+	private static String BINGO_OUTPUT_PATH = "bingo.tsv";
 
 	public static void main(String[] args) throws Exception {
-
+		int bingoNum = 0;
 		if (args == null || args.length == 0) {
 			System.out.println("How many BINGOs? Please enter the required number in the first argument.");
-			return;
+			var scanner = new Scanner(System.in);
+			System.out.print("Input > ");
+			bingoNum = scanner.nextInt();
+			scanner.close();
+		} else {
+			bingoNum = Integer.parseInt(args[0]);
 		}
 
 		var bingoList = new ArrayList<ArrayList<BingoTarget>>();
 
-		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+		for (int i = 0; i < bingoNum; i++) {
 			/** ランダム数（今回は真ん中だけ空欄にする為、24回ランダムでデータを取得する）*/
 			int randamNum = 24;
 			bingoList.add(pick(randamNum));
@@ -62,7 +70,7 @@ public class Bingo5x5App {
 		var allBingoTargets = new ArrayList<BingoTarget>();
 		File bingoTargets = new File("bingo_target.txt");
 		try (
-				var reader = new BufferedReader(new InputStreamReader(new FileInputStream(bingoTargets), "windows-31j"));) {
+				var reader = new BufferedReader(new InputStreamReader(new FileInputStream(bingoTargets), "MS932"));) {
 			String line;
 			while ((line = reader.readLine()) != null) {
 				String[] data = line.split(",", 0); // 行をカンマ区切りで配列に変換
@@ -77,39 +85,44 @@ public class Bingo5x5App {
 	}
 
 	private static void printBingo(ArrayList<ArrayList<BingoTarget>> bingoTargetsList) {
-		for (var bingoTargets : bingoTargetsList) {
-			for (int i = 0; i < 5; i++) {
-				System.out.print(bingoTargets.get(i).targetName + "\t");
+		try {
+			var out = new PrintStream(System.out, true, "MS932");
+			for (var bingoTargets : bingoTargetsList) {
+				for (int i = 0; i < 5; i++) {
+					out.print(bingoTargets.get(i).targetName + "\t");
+				}
+				System.out.println();
+				for (int i = 5; i < 10; i++) {
+					out.print(bingoTargets.get(i).targetName + "\t");
+				}
+				out.println();
+				for (int i = 10; i < 12; i++) {
+					out.print(bingoTargets.get(i).targetName + "\t");
+				}
+				out.print("　\t");
+				for (int i = 12; i < 14; i++) {
+					out.print(bingoTargets.get(i).targetName + "\t");
+				}
+				out.println();
+				for (int i = 14; i < 19; i++) {
+					out.print(bingoTargets.get(i).targetName + "\t");
+				}
+				out.println();
+				for (int i = 19; i < 24; i++) {
+					out.print(bingoTargets.get(i).targetName + "\t");
+				}
+				out.println();
+				out.println();
 			}
-			System.out.println();
-			for (int i = 5; i < 10; i++) {
-				System.out.print(bingoTargets.get(i).targetName + "\t");
-			}
-			System.out.println();
-			for (int i = 10; i < 12; i++) {
-				System.out.print(bingoTargets.get(i).targetName + "\t");
-			}
-			System.out.print("　\t");
-			for (int i = 12; i < 14; i++) {
-				System.out.print(bingoTargets.get(i).targetName + "\t");
-			}
-			System.out.println();
-			for (int i = 14; i < 19; i++) {
-				System.out.print(bingoTargets.get(i).targetName + "\t");
-			}
-			System.out.println();
-			for (int i = 19; i < 24; i++) {
-				System.out.print(bingoTargets.get(i).targetName + "\t");
-			}
-			System.out.println();
-			System.out.println();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
 		}
 	}
 
 	private static void outputBingo(ArrayList<ArrayList<BingoTarget>> bingoTargetsList) {
 		File file = new File(BINGO_OUTPUT_PATH);
 		try (
-				PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "windows-31j")))) {
+				PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "MS932")))) {
 			for (var bingoTargets : bingoTargetsList) {
 				for (int i = 0; i < 5; i++) {
 					writer.print(bingoTargets.get(i).targetName + "\t");
@@ -142,28 +155,28 @@ public class Bingo5x5App {
 		}
 	}
 
-//	/** ＤＢ情報 */
-//	private static String DB_INFO = "jdbc:oracle:thin:@IP:PORT:ORCL";
-//	private static String DB_USER = "user";
-//	private static String DB_PASS = "pass";
-//
-//	/** ビンゴの中に埋める文字を格納しているテーブル。IDには1～シーケンスに登録しておく。 */
-//	private static String BINGO_TARGET_SQL = "SELECT id, target_name FROM BINGO_TARGET_TBL";
-//
-//	private static ArrayList<BingoTarget> getAllFromDbBingoTargets() {
-//		var allBingoTargets = new ArrayList<BingoTarget>();
-//		try {
-//			try (
-//					var con = DriverManager.getConnection(DB_INFO, DB_USER, DB_PASS);
-//					var ps = con.prepareStatement(BINGO_TARGET_SQL);
-//					var result = ps.executeQuery();) {
-//				while (result.next()) {
-//					allBingoTargets.add(new BingoTarget(result.getInt("id"), result.getString("target_name")));
-//				}
-//			}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//		return allBingoTargets;
-//	}
+	//	/** ＤＢ情報 */
+	//	private static String DB_INFO = "jdbc:oracle:thin:@IP:PORT:ORCL";
+	//	private static String DB_USER = "user";
+	//	private static String DB_PASS = "pass";
+	//
+	//	/** ビンゴの中に埋める文字を格納しているテーブル。IDには1～シーケンスに登録しておく。 */
+	//	private static String BINGO_TARGET_SQL = "SELECT id, target_name FROM BINGO_TARGET_TBL";
+	//
+	//	private static ArrayList<BingoTarget> getAllFromDbBingoTargets() {
+	//		var allBingoTargets = new ArrayList<BingoTarget>();
+	//		try {
+	//			try (
+	//					var con = DriverManager.getConnection(DB_INFO, DB_USER, DB_PASS);
+	//					var ps = con.prepareStatement(BINGO_TARGET_SQL);
+	//					var result = ps.executeQuery();) {
+	//				while (result.next()) {
+	//					allBingoTargets.add(new BingoTarget(result.getInt("id"), result.getString("target_name")));
+	//				}
+	//			}
+	//		} catch (SQLException e) {
+	//			e.printStackTrace();
+	//		}
+	//		return allBingoTargets;
+	//	}
 }
